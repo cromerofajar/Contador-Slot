e253fcf3e98a7495080f52d60b2302bf903af2f7
cromerofajar <cromerofajar@danielcastelao.org>
2020-06-14 05:23:00 +0200

    Sin lectura de base de datos

---
+++ contadorslot/BaseDeDatos.java
@@ -0,0 +1,61 @@
+
+package contadorslot;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Caos5000
+ */
+public class BaseDeDatos {
+    public static Connection connect() {
+
+        // parámetro BD
+        String url = "jdbc:sqlite:Carreras.db";
+        Connection conn = null;
+        // Creando conexión a la BD
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    public static void crearTablas(String nomCarrera) {
+
+        String sql = "CREATE TABLE IF NOT EXISTS "+nomCarrera+" (\n"
+                + "	Piloto PRIMARY KEY,\n"
+                + "	Pista number NOT NULL UNIQUE,\n"
+                + "	Vueltas number NOT NULL,\n"
+                + "	Posicion number NOT NULL\n"
+                + ");";
+
+        try (Connection conn = connect();
+                Statement stmt = conn.createStatement()) {
+                stmt.execute(sql);
+                conn.close();
+        } catch (SQLException e) {
+            System.out.println("Error al crear la tabla");
+        }
+        
+    }
+    public static String añadirPiloto(String nomCarrera,String nombre,int pista,int vueltas,int posicion) {
+        String sql = "INSERT INTO "+nomCarrera+"(Piloto,Pista,Vueltas,Posicion) VALUES(?,?,?,?)";
+        try (Connection conn = connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, nombre);
+            pstmt.setInt(2,pista);
+            pstmt.setInt(3,vueltas);
+            pstmt.setInt(4,posicion);
+            pstmt.executeUpdate();
+            return "Piloto "+nombre+" añadido";
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return "Error\nLa pista esta repetida";
+    }
+}
---
+++ contadorslot/ContadorSlot.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contadorslot;
+
+/**
+ *
+ * @author Caos5000
+ */
+public class ContadorSlot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu men= new Menu();
+        men.setVisible(true);
+    }
+    
+}
